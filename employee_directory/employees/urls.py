1c406ac99daa25b1fd6c5710d366314cce95a198
roma <trueget33@gmail.com>
2023-03-13 14:34:51 +0300

    add new template with one worker

--- employee_directory/employees/urls.py
+++ employee_directory/employees/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 # from employees.views import index
 from .views import (
     INdex,
-    AllWorkersListVew
+    AllWorkersListVew,
+    OneWorker
 )
 
 app_name = 'employees'
@@ -11,4 +12,5 @@ urlpatterns = [
     # path('', index, name='index'),
     path('', INdex.as_view(), name='index'),
     path('all_workers/page=<int:page>/', AllWorkersListVew.as_view(), name='all_workers'),
+    path('one_worker/id=<int:pk>/', OneWorker.as_view(), name='one_worker'),
 ]
--- employee_directory/employees/views.py
+++ employee_directory/employees/views.py
@@ -28,7 +28,20 @@ class INdex(View):
 
 
 class AllWorkersListVew(ListView):
+    '''список всех работников'''
     model = Employees
     template_name = 'all_workers.html'
     context_object_name = 'all_workers'
     paginate_by = 500
+
+
+class OneWorker(ListView):
+    '''один работник с подчиненными и руководителем'''
+    model = Employees
+    template_name = 'one_worker.html'
+    context_object_name = 'one_worker'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['worker'] = Employees.objects.get(id=self.kwargs['pk'])
+        return context
